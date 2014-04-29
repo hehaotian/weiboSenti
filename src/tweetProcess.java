d2c900d672f489768ea45310097e55d6c18f4834
Haotian He <haotianh@uw.edu>
2014-04-28 22:55:31 -0700

    c

--- src/tweetProcess.java
+++ src/tweetProcess.java
@@ -7,6 +7,13 @@ import java.util.Map;
 
 public class tweetProcess {
     public static void main(String[] args) throws IOException {
+    	File folder = new File("/Data/619763/");
+    	File[] listOfFiles = folder.listFiles();
 
+    	for (File file : listOfFiles) {
+    		if (file.isFile()) {
+    			System.out.println(file.getName());
+    		}
+    	}
     }
 }
