bca92b9ca2ead97a7ceb2f8d3f7ef3dca4786ec2
Haotian He <haotianh@uw.edu>
2014-05-06 23:17:24 -0700

    c

--- src/get_data.sh
+++ src/get_data.sh
@@ -2,5 +2,5 @@
 
 javac *.java
 java Main $1 $2
-./seg/segment.sh ctb $1 UTF-8 > "$1.seg"
-./seg/segment.sh ctb $2 UTF-8 > "$2.seg"
+./seg/segment.sh ctb $1 UTF-8 0 > "$1.seg"
+./seg/segment.sh ctb $2 UTF-8 0 > "$2.seg"
